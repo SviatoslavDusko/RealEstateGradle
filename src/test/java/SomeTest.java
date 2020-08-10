5a659b44a9aa2a7aad140d811a88237c379bcf53
swiatoslavdusko <swiatikdusko@gmail.com>
2020-08-10 12:57:03 +0300

    passedTestsWithSomeLog

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -1,12 +1,17 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.logging.Logger;
+
 @Test
 public class SomeTest extends BaseTest{
+    private static final Logger logger = Logger.getGlobal();
     @Test
     public void doSomething(){
         System.out.println("bAssert");
+        logger.info("beforeDoSomethingAssert");
         Assert.assertTrue(true);
+        logger.info("afterDoSomethingAssert");
         System.out.println("someTExt");
         System.out.println("aAssert");
     }
