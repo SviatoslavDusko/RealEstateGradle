354dae596e17dbb2f401918ae38afb601071e32c
swiatoslavdusko <swiatikdusko@gmail.com>
2020-08-10 12:50:19 +0300

    Jenkinsfile4withPassedTest3

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,7 @@
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import org.testng.annotations.TestInstance;
 
-import java.security.PrivateKey;
 import java.util.logging.Logger;
 
 public class BaseTest {
