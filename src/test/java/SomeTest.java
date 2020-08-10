a90b9538617334260d8e8a6a8bed06ae5bf11190
swiatoslavdusko <swiatikdusko@gmail.com>
2020-08-10 12:43:02 +0300

    Jenkinsfile4withPassedTest

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -6,7 +6,7 @@ public class SomeTest extends BaseTest{
     @Test
     public void doSomething(){
         System.out.println("bAssert");
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
         System.out.println("someTExt");
         System.out.println("aAssert");
     }
