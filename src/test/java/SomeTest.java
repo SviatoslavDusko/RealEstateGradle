f6d3722057462d4cb8f591cb94d04462451d0691
swiatoslavdusko <swiatikdusko@gmail.com>
2020-08-12 14:20:46 +0300

    failureTest

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -10,6 +10,6 @@ public class SomeTest extends BaseTest {
     @Test
     public void doSomething() {
         logger.info("beforeDoSomethingAssert");
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
     }
 }
